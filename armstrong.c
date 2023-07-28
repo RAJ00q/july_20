5b70c91086289e82cac018dec1e9641b0fee67f7
RAJ00q <139763306+RAJ00q@users.noreply.github.com>
2023-07-28 12:52:26 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int num=153,rem,sum=0;
+    int temp;
+    
+    temp=num;
+    while (num>0)
+    {
+        rem = num%10;
+        sum = sum+rem*rem*rem;
+        num = num/10;
+        
+    }
+      if (temp==sum)
+      {
+          printf("number is armstrong ");
+      }
+      else {
+          printf(" number is not armsstrong");
+      }
+return 0;    
+}
+
